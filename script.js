71093961a2ef3954f48d3d0699d8c848808bcf94
Diego Bueno <diegobuenomendonca@hotmail.com>
2022-02-25 19:38:01 -0300

    Adiciona todos os requisitos exceto requisito 11

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const email = document.getElementById('input-email');
 const senha = document.getElementById('input-senha');
 const button = document.getElementById('button');
+const agree = document.getElementById('agreement');
+const subButton = document.getElementById('submit-btn');
+
+button.addEventListener('click', validarDados);
 
 function validarDados () {
   if (email.value === "tryber@teste.com" && senha.value === '123456') {
@@ -10,4 +14,14 @@ function validarDados () {
   }
 }
 
-button.addEventListener('click', validarDados);
+subButton.disabled = true
+
+agree.addEventListener('click', habilitaBotao)
+
+function habilitaBotao(){
+  if(agree.checked === false) {
+    subButton.disabled = true
+  } else {
+    subButton.disabled = false
+  }
+}
