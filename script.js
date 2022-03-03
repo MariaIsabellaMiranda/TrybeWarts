2f16c09b8256c0b56e2ca9f338a4679b3cc9c0d8
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-03 19:45:17 -0300

    Identaçao de acordo com lint

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const email = document.getElementById('emailLogin');
-const senha = document.getElementById('input-senha');
-const button = document.getElementById('button');
-const agree = document.getElementById('agreement');
-const subButton = document.getElementById('submit-btn');
-const texto = document.getElementById('textarea');
-const contador = document.getElementById('counter');
-const nome = document.getElementById('input-name');
-const sobrenome = document.getElementById('input-lastname');
-const emailForm = document.getElementById('input-email');
-const casas = document.getElementById('house');
-const familias = document.getElementsByName('family');
-const conteudos = document.getElementsByClassName('subject');
-const avaliar = document.getElementsByName('rate');
-const form = document.getElementById('evaluation-form');
-
+const email = document.querySelector('#emailLogin');
+const senha = document.querySelector('#input-senha');
+const button = document.querySelector('#button');
+const agree = document.querySelector('#agreement');
+const subButton = document.querySelector('#submit-btn');
+const texto = document.querySelector('#textarea');
+const contador = document.querySelector('#counter');
+const nome = document.querySelector('#input-name');
+const sobrenome = document.querySelector('#input-lastname');
+const emailForm = document.querySelector('#input-email');
+const casas = document.querySelector('#house');
+const familias = document.querySelectorAll('.family');
+const conteudos = document.querySelectorAll('.subject');
+const avaliar = document.querySelectorAll('.rate');
+const form = document.querySelector('#evaluation-form');
 
 function validarDados() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -24,7 +23,6 @@ function validarDados() {
 }
 button.addEventListener('click', validarDados);
 
-
 // Site usado como base para elaborar botão: https://www.delftstack.com/pt/howto/javascript/javascript-disable-button/#:~:text=A%20melhor%20maneira%20de%20fazer,alternar%20seu%20estado%20on%20%2F%20off%20.
 subButton.disabled = true;
 
@@ -34,87 +32,84 @@ function habilitaBotao() {
   } else {
     subButton.disabled = false;
   }
-};
+}
 agree.addEventListener('click', habilitaBotao);
-
 
 // Site usado como base para elaborar contador: https://pt.stackoverflow.com/questions/25753/como-fazer-um-contador-de-caracteres-de-uma-textarea
 function contaCaractere() {
-  let quant = 500;
-  const total = texto.value.length
+  const quant = 500;
+  const total = texto.value.length;
   if (total <= quant) {
-    let restante = quant - total;
+    const restante = quant - total;
     contador.innerHTML = restante;
-  }else {
   }
-};
+}
 texto.addEventListener('keyup', contaCaractere);
 
 function getNomeESobrenome() {
-  let valueNomeSobrenome = nome.value + " " + sobrenome.value
+  const valueNomeSobrenome = `${nome.value} ${sobrenome.value}`;
   return valueNomeSobrenome;
-};
+}
 
 function getEmail() {
-  let valueEmail = emailForm.value;
+  const valueEmail = emailForm.value;
   return valueEmail;
-};
+}
 
 function getCasas() {
-  let valueCasas = casas.options[casas.selectedIndex].value;
+  const valueCasas = casas.options[casas.selectedIndex].value;
   return valueCasas;
-};
+}
 
 function getFamilias() {
-  for (let i =0; i < familias.length; i += 1) {
-    if(familias[i]:checked) {
-    return familias[i].value;
+  for (let i = 0; i < familias.length; i += 1) {
+    if (familias[i].checked) {
+      return familias[i].value;
     }
   }
-};
+}
 
 function getConteudo() {
-  let valueConteudos = [];
-  for (i = 0; i < conteudos.length; i += 1) {
-    if (conteudos[i]:checked) {
+  const valueConteudos = [];
+  for (let i = 0; i < conteudos.length; i += 1) {
+    if (conteudos[i].checked) {
       valueConteudos.push(conteudos[i].value);
     }
   }
   return valueConteudos.join(', ');
-};
+}
 
 function getAvaliar() {
-  for (let i =0; i < avaliar.length; i += 1) {
-    if(avaliar[i]:checked) {
-    return avaliar[i].value;
+  for (let i = 0; i < avaliar.length; i += 1) {
+    if (avaliar[i].checked) {
+      return avaliar[i].value;
     }
   }
-};
+}
 
 function getTextArea() {
-  let valueTexto = texto.value;
+  const valueTexto = texto.value;
   return valueTexto;
-};
+}
 
 function todosTextos() {
-  let arrayValues = [];
-  arrayValues.push('Nome:' + ' ' + getNomeESobrenome());
-  arrayValues.push('Email:' + ' ' + getEmail());
-  arrayValues.push('Casa:' + ' ' + getCasas());
-  arrayValues.push('Família:' + ' ' + getFamilias());
-  arrayValues.push('Matérias:' + ' ' + getConteudo());
-  arrayValues.push('Avaliação:' + ' ' + getAvaliar());
-  arrayValues.push('Observações:' + ' ' + getTextArea());
+  const arrayValues = [];
+  arrayValues.push(`Nome: ${getNomeESobrenome()}`);
+  arrayValues.push(`Email: ${getEmail()}`);
+  arrayValues.push(`Casa: ${getCasas()}`);
+  arrayValues.push(`Família: ${getFamilias()}`);
+  arrayValues.push(`Matérias: ${getConteudo()}`);
+  arrayValues.push(`Avaliação: ${getAvaliar()}`);
+  arrayValues.push(`Observações: ${getTextArea()}`);
   return arrayValues;
-};
+}
 
-  function createP() {
-    form.innerHTML = '';
-    for (let i = 0; i < todosTextos().length; i += 1) {
-      const p = document.createElement('p');
-      p.innerText = todosTextos()[i];
-      form.appendChild(p);
-    }
+function createP() {
+  form.innerHTML = '';
+  for (let i = 0; i < todosTextos().length; i += 1) {
+    const p = document.createElement('p');
+    p.innerText = todosTextos()[i];
+    form.appendChild(p);
   }
-  subButton.addEventListener('click', createP);
-  
+}
+subButton.addEventListener('click', createP);
